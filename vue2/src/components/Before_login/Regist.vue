7c2efe0bb54c283d08dcf18dfcfe3c0101620f98
Limiun <Limiun_125@163.com>
2020-12-17 20:59:56 +0800

    vue blog

---
+++ vue2/src/components/Before_login/Regist.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-button id="commit" type="primary" v-on:click="commit">登录</el-button>
+</template>
+
+<script>
+    export default {
+      passport: "Regist",
+      methods:{
+        commit(){
+          var passport = this.password
+          this.$axios.get(('http://localhost:8087/backend/access/loginNoPassWord'),passport,{
+          // this.$axios.get(('http://localhost:8087/backend/findPayItemStr'),passport,{
+            }).then(res=>{
+              console.log("请求发送了")
+            })
+          }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue2/src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import router from '../router'
+import Login from "../components/Before_login/Login";
+import HelloWorld from "../components/HelloWorld";
+import video from "../components/Test/video"
+import Regist from "../components/Before_login/Regist"
+Vue.use(Router)
+
+export default new Router({
+
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path:'/Login',
+      name:'Login',
+      component:Login
+    },
+    {
+      path:'/video',
+      name:video,
+      component:video
+    },
+    {
+      path:'/Regist',
+      name:Regist,
+      component:Regist
+    }
+
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+//to到哪儿  from从哪儿离开  next跳转 为空就是放行
+  if (to.path === '/Login') {
+    //如果跳转为登录，就放行
+    next();
+  } else {
+    //取出localStorage判断
+    let token = localStorage.getItem('token');
+    console.log("token"+token)
+    if (token == null || token === '') {
+      console.log('请先登录')
+      // next({name: '/'});
+      next("/Login")
+    } else {
+      next();
+    }
+  }});
+
