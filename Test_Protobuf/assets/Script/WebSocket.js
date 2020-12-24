faf6d30e7468dc1aa10cc295e3239244e9f2301d
Limiun <Limiun_125@163.com>
2020-12-24 21:26:19 +0800

    测试cocos 连接 protobuf

---
+++ Test_Protobuf/assets/Script/WebSocket.js
@@ -0,0 +1,39 @@
+var websocket = {
+	sock:null;
+	on_open function(){
+		let usermeg = 
+		this.send_data();
+		
+	}
+	on_message: function (event) {
+        console.log("client rcv data=" + event.data);
+    },
+ 
+    on_close: function () {
+        this.close();
+    },
+ 
+    on_error: function () {
+        this.close();
+    },
+    
+    close: function () {
+        if(this.sock){
+            this.sock.close();
+            this.sock = null;
+        }
+    },
+	  connect: function (url) {
+        this.sock = new WebSocket(url);
+        this.sock.binaryType = "arraybuffer";
+        this.sock.onopen = this.on_open.bind(this);
+        this.sock.onmessage = this.on_message.bind(this);
+        this.sock.onclose = this.on_close.bind(this);
+        this.sock.onerror = this.on_error.bind(this);
+    },
+	
+	    send_data: function (data) {
+        this.sock.send(data);
+    }
+}
+module.exports = websocket;
